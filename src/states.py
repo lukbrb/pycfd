c46f35161df534669ea1f409a5672b065260dfa9
Célia jacob hinault <celiajacob@Celia.local>
2025-09-14 18:29:19 +0200

    introduce state class

--- src/states.py
+++ src/states.py
@@ -1,208 +1,62 @@
-from dataclasses import dataclass
-from typing import Self
-import params
 import numpy as np
+from typing import Sequence, Union
+from pycfd_types import real_t, VarIndex
+import params
 
-@dataclass
-class State:
-    """
-    Represents the state of the Magnetohydrodynamics system, augmented with the psi field.
-    """
-    r: float
-    u: float
-    v: float
-    w: float
-    p: float
-    bx: float
-    by: float
-    bz: float
-    psi: float
-    
-    def __add__(self, other: Self) -> 'State':
-        return State(
-            self.r + other.r,
-            self.u + other.u,
-            self.v + other.v,
-            self.w + other.w,
-            self.p + other.p,
-            self.bx + other.bx,
-            self.by + other.by,
-            self.bz + other.bz,
-            self.psi + other.psi
-        )
-    
-    def __sub__(self, other: Self) -> 'State':
-        return State(
-            self.r - other.r,
-            self.u - other.u,
-            self.v - other.v,
-            self.w - other.w,
-            self.p - other.p,
-            self.bx - other.bx,
-            self.by - other.by,
-            self.bz - other.bz,
-            self.psi - other.psi
-        )
-    
-    # TODO : interface pour chacun des deux types
-    def __mul__(self, scalar: float|Self) -> 'State':
-        if isinstance(scalar, State):
-            return State(
-                self.r * scalar.r,
-                self.u * scalar.u,
-                self.v * scalar.v,
-                self.w * scalar.w,
-                self.p * scalar.p,
-                self.bx * scalar.bx,
-                self.by * scalar.by,
-                self.bz * scalar.bz,
-                self.psi * scalar.psi
-            )
-        
-        elif isinstance(scalar, (int, float)):
-            return State(
-                self.r * scalar,
-                self.u * scalar,
-                self.v * scalar,
-                self.w * scalar,
-                self.p * scalar,
-                self.bx * scalar,
-                self.by * scalar,
-                self.bz * scalar,
-                self.psi * scalar
-            )
+IR, IU, IV, IW, IP, IE, IBX, IBY, IBZ, IPSI = VarIndex.__members__
 
-def cons_to_prim(u: State) -> State:
-    """
-    Convert conservative variables to primitive variables.
-    
-    Parameters:
-    u (State): The state of the system in conservative variables.
-    
-    Returns:
-    State: The state of the system in primitive variables.
-    """
-    res =  State(
-        r=u.r,
-        u=u.u/u.r,
-        v=u.v/u.r,
-        w=u.w/u.r,
-        p=0,
-        bx=u.bx,
-        by=u.by,
-        bz=u.bz,
-        psi=u.psi
-    )
+class State(np.ndarray):
+    """Classe de base pour PrimState et ConsState."""
 
-    Ek = 0.5 * res.r * (res.u**2 + res.v**2 + res.w**2)
-    Em = 0.5 * (u.bx**2 + u.by**2 + u.bz**2)
-    Epsi = 0.5 * u.psi**2
-    res.p = (u.p - Ek - Em - Epsi) * (params.gamma - 1)
-    return res
+    def __new__(cls, data: Union[Sequence[real_t], None] = None) -> 'State':
+        arr = np.array(data, dtype=real_t) if data else np.zeros(params.Nfields)
+        if arr.shape != (params.Nfields,):
+            raise ValueError(f"Expected {params.Nfields} fields, got {arr.shape}")
+        # vue ndarray avec type State
+        return arr.view(cls)
 
-def prim_to_cons(q: State) -> State:
-    """
-    Convert primitive variables to conservative variables.
-    
-    Parameters:
-    q (State): The state of the system in primitive variables.
-    
-    Returns:
-    State: The state of the system in conservative variables.
-    """
-    Ek = 0.5 * q.r * (q.u**2 + q.v**2 + q.w**2)
-    Em = 0.5 * (q.bx**2 + q.by**2 + q.bz**2)
-    Epsi = 0.5 * q.psi**2
-    res = State(
-        r=q.r,
-        u=q.r * q.u,
-        v=q.r * q.v,
-        w=q.r * q.w,
-        p=Ek + Em + Epsi + q.p / (params.gamma - 1),
-        bx=q.bx,
-        by=q.by,
-        bz=q.bz,
-        psi=q.psi
-    )
-    return res
+    def __array_finalize__(self, obj):
+        # appelé à chaque fois qu’un nouvel ndarray-compat est créé
+        if obj is None:
+            return
 
-def cons_to_prim_array(u: np.ndarray[State], q: np.ndarray[State]) -> np.ndarray[State]:
-    """
-    Convert an array of conservative variables to primitive variables.
-    
-    Parameters:
-    u (np.ndarray[State]): The array of states in conservative variables.
-    
-    Returns:
-    np.ndarray[State]: The array of states in primitive variables.
-    """
-    for i in range(params.Nx):
-        for j in range(params.Ny):
-            q[i, j] = cons_to_prim(u[i, j])
-    return q
 
-def prim_to_cons_array(q: np.ndarray[State], u: np.ndarray[State]) -> np.ndarray[State]:
-    """
-    Convert an array of primitive variables to conservative variables.
-    
-    Parameters:
-    q (np.ndarray[State]): The array of states in primitive variables.
-    
-    Returns:
-    np.ndarray[State]: The array of states in conservative variables.
-    """
-    for i in range(params.Nx):
-        for j in range(params.Ny):
-            u[i, j] = prim_to_cons(q[i, j])
+def primToCons(q: State) -> State:
+    u: State = State()
+    u[IR]   = q[IR]
+    u[IU]   = q[IR] * q[IU]
+    u[IV]   = q[IR] * q[IV]
+    u[IW]   = q[IR] * q[IW]
+    u[IBX]  = q[IBX]
+    u[IBY]  = q[IBY]
+    u[IBZ]  = q[IBZ]
+    u[IPSI] = q[IPSI]
+    Ek = q[IR] * 0.5 * (q[IU]**2 + q[IV]**2 + q[IW]**2)
+    Emag = 0.5 * (q[IBX]**2 + q[IBY]**2 + q[IBZ]**2)
+    Epsi = 0.5 * q[IPSI]**2
+    u[IE] = q[IP]/(params.gamma - 1) + Ek + Emag + Epsi
     return u
 
-def swap_components(q: State, idir: int) -> State:
-    """
-    Swap the components of the state vector.
-    
-    Parameters:
-    q (State): The state of the system.
-    
-    Returns:
-    State: The state with swapped components.
-    """
-    if idir == 0:
-        return State(
-            r=q.r,
-            u=q.u,
-            v=q.v,
-            w=q.w,
-            p=q.p,
-            bx=q.bx,
-            by=q.by,
-            bz=q.bz,
-            psi=q.psi
-        )
-    else:
-        return State(
-            r=q.r,
-            u=q.v,
-            v=q.u,
-            w=q.w,
-            p=q.p,
-            bx=q.by,
-            by=q.bx,
-            bz=q.bz,
-            psi=q.psi
-        )
 
-def compute_hydro_flux(q: State) -> State:
-    Ek: float = 0.5 * q.r * (q.u * q.u + q.v * q.v)
-    E_int: float = (q.p / (params.gamma-1.0) + Ek)
+def consToPrim(u: State) -> State:
+    q: State = State()
+    q[IR]   = u[IR]
+    q[IU]   = u[IU] / u[IR]
+    q[IV]   = u[IV] / u[IR]
+    q[IW]   = u[IW] / u[IR]
+    q[IBX]  = u[IBX]
+    q[IBY]  = u[IBY]
+    q[IBZ]  = u[IBZ]
+    q[IPSI] = u[IPSI]
+    Ek = q[IR] * 0.5 * (q[IU]**2 + q[IV]**2 + q[IW]**2)
+    Emag = 0.5 * (q[IBX]**2 + q[IBY]**2 + q[IBZ]**2)
+    Epsi = 0.5 * q[IPSI]**2
+    q[IP] = (q[IE] - Ek - Emag - Epsi) * (params.gamma -1)
+    return q
 
-    return State(
-      r = q.r*q.u,
-      u = q.r*q.u*q.u + q.p,
-      v = q.r*q.u*q.v,
-      w = 0,
-      p = (q.p + E_int) * q.u,
-      bx = 0,
-      by = 0,
-      bz = 0,
-      psi = 0
-    )
+
+# class PrimState(State):
+#     pass
+
+# class ConsState(State):
+#     pass
